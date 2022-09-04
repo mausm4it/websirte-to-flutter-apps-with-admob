07ce585bb6a613ee7205fd5d4aee3953892fed6b
Md Masum <masum4it@gmail.com>
2022-09-04 08:55:25 +0600

    ok

---
+++ android/app/src/main/kotlin/com/example/webview/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.pawsni.pampers
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/url.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class URL{
+  final playStoreUrl = "";
+  final appStoreUrl = "";
+  final aboutUsUrl="https://www.pampers.com/en-us/about-us";
+  final privacyPolicyUrl="https://privacypolicy.pg.com/en/";
+  final termsConditionsUrl="https://termsandconditions.pg.com/en-us/?web_traffic_source=direct";
+  final webViewUrl="https://www.pampers.com/en-us/baby/sleep/article/how-to-put-a-baby-to-sleep";
+}
